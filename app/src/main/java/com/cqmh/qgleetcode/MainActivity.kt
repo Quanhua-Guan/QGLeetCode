a7fc884ed938ad0ab29a3d680cc4c30749033fe5
yuyuan <yuyuan@gaoding.com>
2022-04-29 20:42:41 +0800

    init

---
+++ app/src/main/java/com/cqmh/qgleetcode/MainActivity.kt
@@ -0,0 +1,150 @@
+package com.cqmh.qgleetcode
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
+
+class Solution1 {
+    fun twoSum(nums: IntArray, target: Int): IntArray {
+        var numMap = mutableMapOf<Int, Int>()
+        var index = 0
+        var result = intArrayOf(0, 0)
+        for (num in nums) {
+            if (numMap.containsKey(target - num)) {
+                result[0] = numMap[target - num]!!
+                result[1] = index
+                break;
+            } else {
+                numMap[num] = index
+            }
+            index++
+        }
+
+        return result;
+    }
+
+    fun twoSum0(nums: IntArray, target: Int): IntArray {
+        var numMap = mutableMapOf<Int, MutableList<Int>>()
+        var index = 0
+        for (num in nums) {
+            if (!numMap.containsKey(num)) {
+                var list = mutableListOf<Int>()
+                list.add(index)
+                numMap[num] = list
+            } else {
+                var list = numMap[num]!!
+                list.add(index)
+            }
+            index++
+        }
+
+        val result = IntArray(2)
+        for (num in nums) {
+            val other = target - num
+            if (numMap.containsKey(num) && numMap.containsKey(other)) {
+                val numList = numMap[num]!!
+                if (num == other) {
+                    if (numList.size >= 2) {
+                        result[0] = numList[0]
+                        result[1] = numList[1]
+                        break;
+                    }
+                } else {
+                    val otherList = numMap[other]!!
+                    result[0] = numList[0]
+                    result[1] = otherList[0]
+                }
+            }
+        }
+
+        return result;
+    }
+}
+
+class ListNode(var `val`: Int) {
+    var next: ListNode? = null
+}
+
+class Solution2 {
+    fun addTwoNumbers(l1: ListNode?, l2: ListNode?): ListNode? {
+        var l1 = l1
+        var l2 = l2
+        var carry = 0 // 进位
+        var result = ListNode(-1)
+        var current = result
+
+        // 1, 2, 3
+        // 2, 3,
+
+        // 1, 2, 3
+        // 1, 2, 3
+
+        // 1, 2
+        // 1, 2, 3
+
+        while (l1 != null || l2 != null) {
+            val val1 = l1?.`val` ?: 0
+            var val2 = l2?.`val` ?: 0
+
+            val sum = val1 + val2 + carry
+            carry = sum / 10
+
+            current.next = ListNode(sum % 10)
+            current = current.next!!
+
+            l1 = l1?.next
+            l2 = l2?.next
+        }
+
+        if (carry > 0) {
+            current.next = ListNode(carry)
+        }
+
+        return result.next
+    }
+}
+
+class Solution9 {
+    fun isPalindrome(x: Int): Boolean {
+        if (x < 0) return false
+
+        val nums = mutableListOf<Int>()
+        var x = x
+        while (x != 0) {
+            nums.add(x % 10)
+            x /= 10
+        }
+
+        var l = 0
+        var r = nums.size - 1
+        while (l < r) {
+            if (nums[l] != nums[r]) return false
+            l++
+            r--
+        }
+
+        return true
+    }
+
+    fun isPalindrome1(x: Int): Boolean {
+        if (x < 0) return false
+
+        // 121 => 12 => 1 => 0
+        // 1   => 12 => 121 =>x
+
+        var xx = x
+        var invertX = 0
+        while (xx != 0) {
+            invertX = invertX * 10 + (xx % 10)
+            xx /= 10
+        }
+
+        return x == invertX
+    }
+}
