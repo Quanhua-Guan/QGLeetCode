9a8a472f58304b04e65d29ccea6d360d43aaaeb6
yuyuan <yuyuan@gaoding.com>
2022-05-12 18:09:44 +0800

    lc141 optimization

--- app/src/main/java/com/cqmh/qgleetcode/MainActivity.kt
+++ app/src/main/java/com/cqmh/qgleetcode/MainActivity.kt
@@ -2510,8 +2510,8 @@ class Solution141 {
         var runner = head
 
         while (walker?.next != null && runner?.next?.next != null) {
-            walker = walker?.next
-            runner = runner?.next?.next
+            walker = walker!!.next
+            runner = runner!!.next!!.next
 
             if (walker == runner) {
                 return true
