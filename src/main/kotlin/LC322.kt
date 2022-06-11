cc5a4955ea493a78262520d46d0eca17daa3e0dd
yuyuan <yuyuan@gaoding.com>
2022-06-11 12:02:28 +0800

    lc322

--- src/main/kotlin/LC322.kt
+++ src/main/kotlin/LC322.kt
@@ -9,6 +9,7 @@ class LC322 {
                 if (remain == 0) return 0
                 if (mem[remain] != 0) return mem[remain]
 
+                // dp[amount] = minOf{ dp[amount - c] + 1 }
                 var min = Int.MAX_VALUE
                 for (c in coins) {
                     // 选择1个c
@@ -22,4 +23,25 @@ class LC322 {
             return coinChange(amount, IntArray(amount + 1))
         }
     }
+
+    /// 322. 零钱兑换
+    class Solution322_DP {
+        fun coinChange(coins: IntArray, amount: Int): Int {
+            // 凑成 remain 数额需要使用的最小硬币数
+            // quantity[i] = 使用 coins 面额凑成 i 的最小硬币个数
+            // quantity[i] = minOf{ quantity[i - c] + 1 }
+            val quantity = IntArray(amount + 1) { amount + 1 } // quantity[i] 最大值为 amount, 取 >=amount + 1 则说明无法凑整.
+            quantity[0] = 0
+            for (coinAmount in 1 until amount + 1) {
+                for (coin in coins) {
+                    if (coin <= coinAmount) {
+                        quantity[coinAmount] = minOf(quantity[coinAmount], quantity[coinAmount - coin] + 1)
+                    }
+                }
+            }
+            if (quantity[amount] > amount) return -1
+            return quantity[amount]
+        }
+    }
+
 }
